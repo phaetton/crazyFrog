3612cb19531fbf52c538b8ed1a5b951786363cfa
phaetton <31552767+phaetton@users.noreply.github.com>
2023-03-14 01:09:58 -0600

    personaje mueve
    Derecha e izquierda

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+public float runSpeed=2;
+public float jumpSpeed=3;
+Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D=GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey("d") || Input.GetKey("right")) {
+            rb2D.velocity= new Vector2(runSpeed, rb2D.velocity.y);
+        }
+        else if(Input.GetKey("a") || Input.GetKey("left")) {
+            rb2D.velocity= new Vector2(-runSpeed, rb2D.velocity.y);
+        }
+        else{
+            rb2D.velocity=new Vector2(0, rb2D.velocity.y);
+        }
+    }
+}
