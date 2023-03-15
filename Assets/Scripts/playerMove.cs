292e99b90e97f392ac8ef42ced554bed1fe58f14
phaetton <31552767+phaetton@users.noreply.github.com>
2023-03-15 13:53:42 -0600

    Salto funcionando
    Animación de salto

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -40,25 +40,40 @@ public float lowJumpMultiplier = 1f;
             //gira a la izquierda
             spriteRenderer.flipX = true;
             //animación correr
-
             animator.SetBool("Run", true);
         }
         else{
+            //no tecla presionada
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
             //sin animación correr
             animator.SetBool("Run", false);
         }
 
-        if(Input.GetKeyUp("w") &&  checkGround.isGrounded) {
+        if((Input.GetKeyUp("w") || Input.GetKeyUp("space") || Input.GetKeyUp("up")) &&  checkGround.isGrounded) {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
         }
 
+        //esta en el suelo
+        if(checkGround.isGrounded == true) {
+            animator.SetBool("Jump", false);
+        } 
+       /* if (checkGround.isGrounded == false)
+        {
+        }
+       */
         if(betterJump){
             if(rb2D.velocity.y<0){
                 rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallJumMultiplier) * Time.deltaTime;
+                animator.SetBool("Jump", true);
+                animator.SetBool("Run", false);
             }
-            if(rb2D.velocity.y>0 && !Input.GetKey("space")){
+            if(rb2D.velocity.y>0 && !(Input.GetKeyUp("w") || Input.GetKeyUp("space") || Input.GetKeyUp("up")))
+            {
                 rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+                animator.SetBool("Jump", true);
+                animator.SetBool("Run", false);
             }
         }
     }
