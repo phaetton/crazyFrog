34c3e034d58967ca34b883c01482c84aac8b0d40
phaetton <31552767+phaetton@users.noreply.github.com>
2023-03-19 10:13:33 -0600

    enemigos con pinchos
    agregado enemigo que se mueve y pincho, los dos destruyen al jugador

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -49,25 +49,26 @@ public float lowJumpMultiplier = 1f;
             animator.SetBool("Run", false);
         }
 
-        if((Input.GetKeyUp("w") || Input.GetKeyUp("space") || Input.GetKeyUp("up")) &&  checkGround.isGrounded) {
+        if((Input.GetKeyUp("w") || Input.GetKeyUp("space") || Input.GetKeyUp("up")) ) {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
             animator.SetBool("Jump", true);
-            animator.SetBool("Run", false);
         }
 
         //esta en el suelo
         if(checkGround.isGrounded == true) {
             animator.SetBool("Jump", false);
         } 
-       /* if (checkGround.isGrounded == false)
+        if (checkGround.isGrounded == false)
         {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+
         }
-       */
-        if(betterJump){
+
+        if (betterJump){
             if(rb2D.velocity.y<0){
                 rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallJumMultiplier) * Time.deltaTime;
                 animator.SetBool("Jump", true);
-                animator.SetBool("Run", false);
             }
             if(rb2D.velocity.y>0 && !(Input.GetKeyUp("w") || Input.GetKeyUp("space") || Input.GetKeyUp("up")))
             {
